02cce27a865b3e0619f317a7bcf70d3e128d887a
Vitor Filardi <rabelo51@gmail.com>
2020-05-31 16:04:27 -0300

    Create Data Context and modelBuilder configs

---
+++ BudgetAppAPI/Repositories/DataContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YabaAPI.Models;
+
+namespace YabaAPI.Repositories
+{
+	public class DataContext : DbContext
+	{
+		public DbSet<Transaction> Transactions { get; set; }
+
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{	}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Transaction>()
+				.ToTable("Transactions")
+				.HasKey(t => t.Id);
+
+
+			modelBuilder.Entity<Transaction>()
+				.Property(t => t.Id)
+				.HasColumnName("TR_Id")
+				.ValueGeneratedOnAdd()
+				.IsRequired();
+
+			modelBuilder.Entity<Transaction>()
+				.Property(t => t.Origin)
+				.HasColumnName("TR_Origin")
+				.HasMaxLength(100);
+
+			modelBuilder.Entity<Transaction>()
+				.Property(t => t.Amount)
+				.HasColumnName("TR_Amount")
+				.HasColumnType("decimal(12,2)")
+				.IsRequired();
+
+			modelBuilder.Entity<Transaction>()
+				.Property(t => t.Date)
+				.HasColumnName("TR_Date")
+				.HasColumnType("date")
+				.IsRequired();
+		}
+	}
+}
