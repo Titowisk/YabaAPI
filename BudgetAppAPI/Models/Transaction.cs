c38b066624da11c98125538ef4ab037450273570
Vitor Filardi <rabelo51@gmail.com>
2020-05-31 16:03:08 -0300

    Create entity Transaction

---
+++ BudgetAppAPI/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YabaAPI.Models
+{
+	public class Transaction
+	{
+		public long Id { get; private set; }
+		public string? Origin { get; private set; }
+		public DateTime Date { get; private set; }
+		public decimal Amount { get; private set; }
+	}
+}
