2006ad7cbd769a28e16040a1b16572a3df71925a
Vitor Filardi <rabelo51@gmail.com>
2020-06-11 12:34:30 -0300

    Add contructor with parameters and empty to Transaction model

--- BudgetAppAPI/Models/Transaction.cs
+++ BudgetAppAPI/Models/Transaction.cs
@@ -4,9 +4,21 @@ namespace YabaAPI.Models
 {
 	public class Transaction
 	{
-		public long Id { get; private set; }
-		public string? Origin { get; private set; }
-		public DateTime Date { get; private set; }
-		public decimal Amount { get; private set; }
+		public Transaction(string origin, DateTime date, decimal amount)
+		{
+			Origin = origin;
+			Date = date;
+			Amount = amount;
+		}
+
+		public Transaction() 
+		{
+			// System.NotSupportedException: Deserialization of reference types without parameterless constructor is not supported. Type 'YabaAPI.Models.Transaction'
+		}
+
+		public long Id { get; set; }
+		public string? Origin { get; set; }
+		public DateTime Date { get; set; }
+		public decimal Amount { get; set; }
 	}
 }
