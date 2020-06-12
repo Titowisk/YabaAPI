45001737c55690f80a0487e9b1102f28e82dd0ba
Vitor Filardi <rabelo51@gmail.com>
2020-06-12 20:23:01 -0300

    Create BankAccount Model

---
+++ BudgetAppAPI/Models/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace YabaAPI.Models
+{
+	public class BankAccount
+	{
+		public BankAccount(string number, string agency)
+		{
+			Number = number;
+			Agency = agency;
+		}
+
+		public int Id { get; set; }
+		public string Number { get; set; }
+		public string Agency { get; set; }
+		public IEnumerable<Transaction>? Transactions { get; set; }
+	}
+}
