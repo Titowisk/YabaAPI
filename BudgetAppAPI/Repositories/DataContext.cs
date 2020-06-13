12126c09372b10a2be2950217d7d6594d6605200
Vitor Filardi <rabelo51@gmail.com>
2020-06-12 21:24:36 -0300

    Add separated class configurations

--- BudgetAppAPI/Repositories/DataContext.cs
+++ BudgetAppAPI/Repositories/DataContext.cs
@@ -13,57 +13,9 @@ namespace YabaAPI.Repositories
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			modelBuilder.Entity<Transaction>()
-				.ToTable("Transactions")
-				.HasKey(t => t.Id);
+			modelBuilder.ApplyConfiguration(new TransactionConfiguration());
 
-
-			modelBuilder.Entity<Transaction>()
-				.Property(t => t.Id)
-				.HasColumnName("TR_Id")
-				.ValueGeneratedOnAdd()
-				.IsRequired();
-
-			modelBuilder.Entity<Transaction>()
-				.Property(t => t.Origin)
-				.HasColumnName("TR_Origin")
-				.HasMaxLength(100);
-
-			modelBuilder.Entity<Transaction>()
-				.Property(t => t.Amount)
-				.HasColumnName("TR_Amount")
-				.HasColumnType("decimal(12,2)")
-				.IsRequired();
-
-			modelBuilder.Entity<Transaction>()
-				.Property(t => t.Date)
-				.HasColumnName("TR_Date")
-				.HasColumnType("date")
-				.IsRequired();
-
-			// Too much code in this DataContext
-			modelBuilder.Entity<BankAccount>()
-				.ToTable("BankAccounts")
-				.HasKey(bk => bk.Id);
-
-			modelBuilder.Entity<BankAccount>()
-				.HasMany(bk => bk.Transactions)
-				.WithOne(t => t.BankAccount);
-
-			modelBuilder.Entity<BankAccount>()
-				.Property(bk => bk.Id)
-				.HasColumnName("BK_Id")
-				.IsRequired();
-
-			modelBuilder.Entity<BankAccount>()
-				.Property(bk => bk.Number)
-				.HasColumnName("BK_Number")
-				.IsRequired();
-
-			modelBuilder.Entity<BankAccount>()
-				.Property(bk => bk.Agency)
-				.HasColumnName("BK_Agency")
-				.IsRequired();
+			modelBuilder.ApplyConfiguration(new BankAccountConfiguration());
 		}
 	}
 }
---
+++ BudgetAppAPI/Repositories/TransactionConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YabaAPI.Models;
+
+namespace YabaAPI.Repositories
+{
+	public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+	{
+		public void Configure(EntityTypeBuilder<Transaction> builder)
+		{
+			builder
+				.ToTable("Transactions")
+				.HasKey(t => t.Id);
+
+
+			builder
+				.Property(t => t.Id)
+				.HasColumnName("TR_Id")
+				.ValueGeneratedOnAdd()
+				.IsRequired();
+
+			builder
+				.Property(t => t.Origin)
+				.HasColumnName("TR_Origin")
+				.HasMaxLength(100);
+
+			builder
+				.Property(t => t.Amount)
+				.HasColumnName("TR_Amount")
+				.HasColumnType("decimal(12,2)")
+				.IsRequired();
+
+			builder
+				.Property(t => t.Date)
+				.HasColumnName("TR_Date")
+				.HasColumnType("date")
+				.IsRequired();
+		}
+	}
+}
