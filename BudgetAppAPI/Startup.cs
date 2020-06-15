1f220d9e2c9e597a11a409a9b998e71b5ab9f973
Vitor Filardi <rabelo51@gmail.com>
2020-06-15 20:11:11 -0300

    Add ReferenceLoopHandling.Ignore

--- BudgetAppAPI/Startup.cs
+++ BudgetAppAPI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using YabaAPI.Repositories;
+using Newtonsoft.Json;
 
 namespace YabaAPI
 {
@@ -20,7 +21,8 @@ namespace YabaAPI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllers();
+			services.AddControllers()
+				.AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
 			services.AddDbContext<DataContext>(opt =>
 				opt.UseSqlServer(Configuration.GetConnectionString("SqlServerDB")));
 		}
