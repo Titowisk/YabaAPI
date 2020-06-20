ac53db10050ea988b488f4f013785b34571df95e
Vitor Filardi <rabelo51@gmail.com>
2020-06-20 17:56:42 -0300

    Add class BankCode

---
+++ BudgetAppAPI/Models/BankCode.cs
@@ -0,0 +1,16 @@
+﻿using YabaAPI.Abstracts;
+
+namespace YabaAPI.Models
+{
+	public class BankCode : Enumeration<short>
+	{
+		public static readonly BankCode BRADESCO = new BankCode(237, "Bradesco");
+
+		private BankCode(short value, string name)
+			: base(value, name)
+		{
+		}
+	}
+
+	// https://contasimples.com/blog/lista-de-codigos-dos-bancos/
+}
