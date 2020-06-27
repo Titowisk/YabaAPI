934b46381d4c9f49f9383cb6beeea7452f3bd970
Vitor Filardi <rabelo51@gmail.com>
2020-06-27 20:52:57 -0300

    Ctrl K E

--- BudgetAppAPI/Controllers/CsvReaderController.cs
+++ BudgetAppAPI/Controllers/CsvReaderController.cs
@@ -8,118 +8,116 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using YabaAPI.Abstracts;
 using YabaAPI.Models;
-using YabaAPI.Repositories;
 using YabaAPI.Repositories.Contracts;
 
 namespace YabaAPI.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class CsvReaderController : ControllerBase
-	{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsvReaderController : ControllerBase
+    {
         private readonly ITransactionRepository _transactionRepository;
         private readonly IBankAccountRepository _bankAccountRepository;
 
         public CsvReaderController(
-			ITransactionRepository transactionRepository,
-			IBankAccountRepository bankAccountRepository)
+            ITransactionRepository transactionRepository,
+            IBankAccountRepository bankAccountRepository)
         {
-			_transactionRepository = transactionRepository;
-			_bankAccountRepository = bankAccountRepository;
-		}
+            _transactionRepository = transactionRepository;
+            _bankAccountRepository = bankAccountRepository;
+        }
 
-		[Route("[Action]")]
-		[HttpPost]
-		public async Task<ActionResult<string>> ReadCsvFileFromForm(IFormFileCollection csvFiles, [FromForm] short bankCode)
-		{
-			// TODO: validate csv file
+        [Route("[Action]")]
+        [HttpPost]
+        public async Task<ActionResult<string>> ReadCsvFileFromForm(IFormFileCollection csvFiles, [FromForm] short bankCode)
+        {
+            // TODO: validate csv file
 
-			// TODO: validate bankCode exists in BankCode Enumeration Class
+            // TODO: validate bankCode exists in BankCode Enumeration Class
 
-			// TODO: dinamically choose which BankReader to use for reading
+            // TODO: dinamically choose which BankReader to use for reading
 
-			foreach (var csv in csvFiles)
-			{
-				var parsedFile = BradescoReader(csv);
+            foreach (var csv in csvFiles)
+            {
+                var parsedFile = BradescoReader(csv);
 
-				SaveTransactions(parsedFile, bankCode);
-			}
-			
-			// TODO: custom error handling for each batch of transactions saved
+                SaveTransactions(parsedFile, bankCode);
+            }
 
-			// TODO: return number of files processed and status for each one
-			return "olá";
-		}
+            // TODO: custom error handling for each batch of transactions saved
+
+            // TODO: return number of files processed and status for each one
+            return "olá";
+        }
 
 
 
         #region Priv Methods
         private void SaveTransactions(BradescoCsvFile parsedFile, short bankCode)
         {
-			var splitFirstLine = parsedFile.FirstLine.Split("|");
-			var agencyNumber = splitFirstLine[0].Split(":")[2].Trim();
-			var accountNumber = splitFirstLine[1].Split(":")[1].Trim().Replace("-", "");
-			var enumBankCode = BankCode.FromValue<BankCode>(bankCode);
+            var splitFirstLine = parsedFile.FirstLine.Split("|");
+            var agencyNumber = splitFirstLine[0].Split(":")[2].Trim();
+            var accountNumber = splitFirstLine[1].Split(":")[1].Trim().Replace("-", "");
+            var enumBankCode = BankCode.FromValue<BankCode>(bankCode);
 
-			var newBankAccount = new BankAccount(accountNumber, agencyNumber, enumBankCode);
+            var newBankAccount = new BankAccount(accountNumber, agencyNumber, enumBankCode);
 
-			var bankAccount = _bankAccountRepository.Find(newBankAccount);
-			if (bankAccount == null)
+            var bankAccount = _bankAccountRepository.Find(newBankAccount);
+            if (bankAccount == null)
             {
-				bankAccount = newBankAccount;
-			}
+                bankAccount = newBankAccount;
+            }
 
-			foreach (var data in parsedFile.BradescoTransactions)
-			{
+            foreach (var data in parsedFile.BradescoTransactions)
+            {
                 try
                 {
-					var amount = string.IsNullOrEmpty(data.Credito) ? data.Debito : data.Credito;
+                    var amount = string.IsNullOrEmpty(data.Credito) ? data.Debito : data.Credito;
 
-					var newTransaction = new Transaction(
-						data.Origem,
-						DateTime.Parse(data.Data),
-						decimal.Parse(amount));
-				
-					bankAccount.Transactions.Add(newTransaction);
+                    var newTransaction = new Transaction(
+                        data.Origem,
+                        DateTime.Parse(data.Data),
+                        decimal.Parse(amount));
+
+                    bankAccount.Transactions.Add(newTransaction);
                 }
                 catch (Exception ex)
                 {
 
                 }
 
-			}
+            }
 
-			// TODO: create unitOfWork to assure atomicity in persisting
-			if (bankAccount.Id > 0)
-				_bankAccountRepository.Update(bankAccount).Wait();
-			else
-				_bankAccountRepository.Create(bankAccount).Wait();
-			// TODO: change to async Task
-		}
+            // TODO: create unitOfWork to assure atomicity in persisting
+            if (bankAccount.Id > 0)
+                _bankAccountRepository.Update(bankAccount).Wait();
+            else
+                _bankAccountRepository.Create(bankAccount).Wait();
+            // TODO: change to async Task
+        }
 
         private BradescoCsvFile BradescoReader(IFormFile csv)
-		{
-			// TODO: change to async Task
-			var bradescoCsv = new BradescoCsvFile();
+        {
+            // TODO: change to async Task
+            var bradescoCsv = new BradescoCsvFile();
 
-			using var reader = new StreamReader(csv.OpenReadStream());
-			using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
-			{
-				csvReader.Configuration.Delimiter = ";";
-				csvReader.Configuration.MissingFieldFound = null; // TODO: this removes the need to use the try/catch block (test)
-				csvReader.Configuration.IgnoreBlankLines = false;
+            using var reader = new StreamReader(csv.OpenReadStream());
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csvReader.Configuration.Delimiter = ";";
+                csvReader.Configuration.MissingFieldFound = null; // TODO: this removes the need to use the try/catch block (test)
+                csvReader.Configuration.IgnoreBlankLines = false;
 
-				while (csvReader.Read())
-				{
-					var rowIndex = csvReader.Context.Row;
-					if(rowIndex > 3)
+                while (csvReader.Read())
+                {
+                    var rowIndex = csvReader.Context.Row;
+                    if (rowIndex > 3)
                     {
                         try
                         {
-							if(string.IsNullOrEmpty(csvReader.Context.RawRecord))
-								break;
+                            if (string.IsNullOrEmpty(csvReader.Context.RawRecord))
+                                break;
 
                             if (int.TryParse(csvReader.GetField(2), out int i))
                             {
@@ -141,50 +139,50 @@ namespace YabaAPI.Controllers
                         }
                         catch (Exception ex)
                         {
-							    
+
                         }
 
-					}
-					else if (rowIndex == 1)
-					{
-						bradescoCsv.FirstLine = csvReader.GetField(0);
-					}
-					
-				}
-			};
+                    }
+                    else if (rowIndex == 1)
+                    {
+                        bradescoCsv.FirstLine = csvReader.GetField(0);
+                    }
 
-			return bradescoCsv;
-		}
+                }
+            };
 
-		#endregion
-	}
+            return bradescoCsv;
+        }
 
-	public class BradescoCsvFile
-	{
+        #endregion
+    }
+
+    public class BradescoCsvFile
+    {
         public BradescoCsvFile()
         {
-			BradescoTransactions = new List<BradescoTransaction>();
-		}
+            BradescoTransactions = new List<BradescoTransaction>();
+        }
 
-		public string FirstLine { get; set; } // Extrato de: Ag: 1425 | Conta: 711093-6
-		public string Header { get; set; } // Data;Hist�rico;Docto.;Cr�dito (R$);D�bito (R$);Saldo (R$);
+        public string FirstLine { get; set; } // Extrato de: Ag: 1425 | Conta: 711093-6
+        public string Header { get; set; } // Data;Hist�rico;Docto.;Cr�dito (R$);D�bito (R$);Saldo (R$);
 
-		public List<BradescoTransaction> BradescoTransactions { get; }
-	}
+        public List<BradescoTransaction> BradescoTransactions { get; }
+    }
 
-	public class BradescoTransaction
-	{
-		public string Data { get; set; }
-		public string Historico { get; set; }
-		public string Docto { get; set; }
-		public string Credito { get; set; }
-		public string Debito { get; set; }
-		public string Saldo { get; set; }
+    public class BradescoTransaction
+    {
+        public string Data { get; set; }
+        public string Historico { get; set; }
+        public string Docto { get; set; }
+        public string Credito { get; set; }
+        public string Debito { get; set; }
+        public string Saldo { get; set; }
         public string Origem { get; set; }
 
     }
 
-	/*NOTES
+    /*NOTES
 	 * Class is to big. There is a clear need of breaking down in more classes
 	 * // TODO: check if docto number exists in DB ??
 	 */
