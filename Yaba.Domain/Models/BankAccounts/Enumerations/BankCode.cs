e6261929df165c20b2d58ccf1eec32d76f9e9b3e
Vitor Filardi <rabelo51@gmail.com>
2020-07-02 15:37:17 -0300

    Add BankAccount

---
+++ Yaba.Domain/Models/BankAccounts/Enumerations/BankCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Yaba.Domain.Models.Abstracts;
+using Yaba.Tools.Validations;
+
+namespace Yaba.Domain.Models.BankAccounts.Enumerations
+{
+    public class BankCode : Enumeration
+    {
+        public static readonly BankCode BRADESCO = new BankCode(237, "Bradesco");
+
+        private BankCode(short value, string name)
+            : base(value, name)
+        {
+        }
+
+        public static void ValidateCode(short value)
+        {
+            var valueExists = BankCode.GetAll<BankCode>().Any(item => item.Value == value);
+
+            Validate.IsTrue(valueExists, $"Bank, with code {value}, not registered yet");
+        }
+    }
+}
---
+++ Yaba.Domain/Models/BankAccounts/IBankAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Yaba.Domain.Models.BankAccounts;
+
+namespace Yaba.Domain.Models
+{
+    public interface IBankAccountRepository : IAsyncDisposable
+    {
+        Task<IEnumerable<BankAccount>> GetAll();
+        Task<BankAccount> GetById(int id);
+        BankAccount? Find(BankAccount bankAccount);
+        Task Update(BankAccount entity);
+        Task Delete(int id);
+        Task Delete(BankAccount id);
+        Task Create(BankAccount entity);
+        Task<bool> Exists(int id);
+    }
+}
