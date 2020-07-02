6c6db2ee21d6597bc173c10b83859ea7f0075667
Vitor Filardi <rabelo51@gmail.com>
2020-07-02 17:47:49 -0300

    Add Migrations

---
+++ Yaba.Infrastructure.Persistence/Migrations/20200613001229_CreateBankAccountEntity.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Yaba.Infrastructure.Persistence.Migrations
+{
+    public partial class CreateBankAccountEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BankAccountId",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    BK_Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BK_Number = table.Column<string>(nullable: false),
+                    BK_Agency = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.BK_Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_BankAccountId",
+                table: "Transactions",
+                column: "BankAccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_BankAccounts_BankAccountId",
+                table: "Transactions",
+                column: "BankAccountId",
+                principalTable: "BankAccounts",
+                principalColumn: "BK_Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_BankAccounts_BankAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_BankAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "BankAccountId",
+                table: "Transactions");
+        }
+    }
+}
