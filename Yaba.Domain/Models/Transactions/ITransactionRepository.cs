687b64ae9f93fb79e36c066ebb3fb619b22d84a9
Vitor Filardi <rabelo51@gmail.com>
2020-07-02 15:43:22 -0300

    Add Transactions

---
+++ Yaba.Domain/Models/Transactions/ITransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Yaba.Domain.Models.Transactions
+{
+    public interface ITransactionRepository
+    {
+        Transaction GetById(long id);
+        Transaction GetByIdWithBankAccount(long id);
+        IEnumerable<Transaction> GetAll();
+        void Create(Transaction entity);
+        void Update(Transaction entity);
+        void Delete(long id);
+        void Delete(Transaction entity);
+    }
+}
---
+++ Yaba.Domain/Models/Transactions/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Yaba.Domain.Models.BankAccounts;
+
+namespace Yaba.Domain.Models.Transactions
+{
+	public class Transaction
+	{
+		public Transaction(string origin, DateTime date, decimal amount)
+		{
+			Origin = origin;
+			Date = date;
+			Amount = amount;
+		}
+
+		public Transaction() 
+		{
+			// System.NotSupportedException: Deserialization of reference types without parameterless constructor is not supported. Type 'YabaAPI.Models.Transaction'
+		}
+
+		public long Id { get; private set; }
+		public string? Origin { get; private set; }
+		public DateTime Date { get; private set; }
+		public decimal Amount { get; private set; }
+		public int? BankAccountId { get; private set; }
+		public BankAccount? BankAccount { get; set; }
+		public Category? Category { get; set; }
+	}
+}
