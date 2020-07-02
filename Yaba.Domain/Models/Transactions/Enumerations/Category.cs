fc7f6fbc1167cd5c2f980a29289fd13cb304e5e4
Vitor Filardi <rabelo51@gmail.com>
2020-07-02 15:45:12 -0300

    Add enum Category of Transactions

---
+++ Yaba.Domain/Models/Transactions/Enumerations/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace Yaba.Domain.Models.Transactions.Enumerations
+{
+	public enum Category : short
+	{
+		HomeExpenses = 1,
+		Transportation = 2,
+		Food = 3,
+		Clothing = 4,
+		Healthcare = 5,
+		Entertainment = 6,
+		Education = 7,
+		Savings = 8,
+		Personal = 9
+	}
+}
