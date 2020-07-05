6426edff612794464849ad25bdaff7ede433277b
Vitor Filardi <rabelo51@gmail.com>
2020-07-05 14:17:36 -0300

    Create contract for different bank readers implementations

---
+++ Yaba.Application/BankStatementReaders/IBankEstatementReader.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Yaba.Infrastructure.DTO;
+
+namespace Yaba.Application.BankStatementReaders
+{
+    public interface IBankEstatementReader
+    {
+        StandardBankStatementDTO ProcessBankInformation(IFormFile csvFile);
+
+        // TODO: validate csv file as a valid bankstatement based on an existent bankCode
+    }
+}
