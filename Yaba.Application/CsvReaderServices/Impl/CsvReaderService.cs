b21a13712ddc9026be081bb2c39502cc7bf41862
Vitor Filardi <rabelo51@gmail.com>
2020-07-05 14:19:24 -0300

    Create PersistBankEstatementInformation in CsvReaderService

--- Yaba.Application/CsvReaderServices/Impl/CsvReaderService.cs
+++ Yaba.Application/CsvReaderServices/Impl/CsvReaderService.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Yaba.Application.BankStatementReaders;
 using Yaba.Domain.Models.BankAccounts;
+using Yaba.Domain.Models.BankAccounts.Enumerations;
+using Yaba.Domain.Models.Transactions;
 using Yaba.Infrastructure.DTO;
+using Yaba.Tools.Validations;
 
 namespace Yaba.Application.CsvReaderServices.Impl
 {
@@ -18,25 +21,75 @@ namespace Yaba.Application.CsvReaderServices.Impl
             _bankAccountRepository = bankAccountRepository;
         }
 
-        public Task<IEnumerable<FileStatusDTO>> ReadTransactionsFromFiles(IFormFileCollection csvFiles, short bankCode)
+        public async Task<IEnumerable<FileStatusDTO>> ReadTransactionsFromFiles(IFormFileCollection csvFiles, short bankCode)
         {
             IBankEstatementReader reader = BankStatementReaderFactory.GetReader(bankCode);
+            var fileStatusResult = new List<FileStatusDTO>();
 
             foreach (var csv in csvFiles)
             {
+                var fileStatus = new FileStatusDTO();
+
                 try
                 {
-                    // TODO: validate file
+                    Validate.IsTrue(csv.ContentType == "text/csv", "Only csv files are accepted");
+
+                    Validate.IsTrue(csv.Length > 0, $"File {csv.FileName} is empty");
 
                     var parsedFile = reader.ProcessBankInformation(csv);
 
-                    PersistBankEstatementInformation(parsedFile);
+                    await PersistBankEstatementInformation(parsedFile, bankCode);
+
+                    fileStatus.IsSuccessfullRead = true;
                 }
                 catch (Exception)
                 {
                     // TODO: logger
+                    fileStatus.IsSuccessfullRead = false;
+                }
+                finally
+                {
+                    fileStatusResult.Add(fileStatus);
                 }
             }
+
+            return fileStatusResult;
+        }
+
+        private async Task PersistBankEstatementInformation(StandardBankStatementDTO parsedFile, short bankCode)
+        {
+            var enumBankCode = BankCode.FromValue<BankCode>(bankCode);
+
+            var newBankAccount = new BankAccount(parsedFile.AccountNumber, parsedFile.AgencyNumber, enumBankCode);
+
+            var bankAccount = _bankAccountRepository.Find(newBankAccount);
+            if (bankAccount == null)
+            {
+                bankAccount = newBankAccount;
+            }
+
+            foreach (var data in parsedFile.Transactions)
+            {
+                try
+                {
+                    // TODO: create mapping between Transaction and StandardTransaction
+                    var newTransaction = new Transaction(
+                        data.Origin,
+                        data.Date,
+                        data.Amount);
+
+                    bankAccount.Transactions.Add(newTransaction);
+                }
+                catch (Exception)
+                {
+                    // TODO: add logger ?
+                }
+            }
+
+            if (bankAccount.Id > 0)
+                await _bankAccountRepository.Update(bankAccount);
+            else
+                await _bankAccountRepository.Create(bankAccount);
         }
     }
 }
