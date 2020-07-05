56506b4c395079682864a186d58bb995c7cec1d7
Vitor Filardi <rabelo51@gmail.com>
2020-07-05 15:26:09 -0300

    Add nullable to some properties of FIleStatusDTO

--- Yaba.Infrastructure.DTO/FileStatusDTO.cs
+++ Yaba.Infrastructure.DTO/FileStatusDTO.cs
@@ -6,8 +6,8 @@ namespace Yaba.Infrastructure.DTO
     {
         public string FileName { get; set; }
         public bool IsSuccessfullRead { get; set; }
-        public int TransactionsSaved { get; set; }
-        public DateTime InitialDate { get; set; }
-        public DateTime FinalDate { get; set; }
+        public int? TransactionsSaved { get; set; }
+        public DateTime? InitialDate { get; set; }
+        public DateTime? FinalDate { get; set; }
     }
 }
