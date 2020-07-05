30cafbf20070d348a491248e8106566044bf9f30
Vitor Filardi <rabelo51@gmail.com>
2020-07-05 11:33:41 -0300

    Create CsvReaderService

---
+++ Yaba.Application/CsvReaderServices/ICsvReaderService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Yaba.Infrastructure.DTO;
+
+namespace Yaba.Application.CsvReaderServices
+{
+    public interface ICsvReaderService
+    {
+        Task<IEnumerable<FileStatusDTO>> ReadTransactionsFromFiles(IFormFileCollection csvFiles, short bankCode);
+    }
+}
---
+++ Yaba.Application/CsvReaderServices/Impl/CsvReaderService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Yaba.Application.BankStatementReaders;
+using Yaba.Domain.Models.BankAccounts;
+using Yaba.Infrastructure.DTO;
+
+namespace Yaba.Application.CsvReaderServices.Impl
+{
+    public class CsvReaderService : ICsvReaderService
+    {
+        private readonly IBankAccountRepository _bankAccountRepository;
+
+        public CsvReaderService(IBankAccountRepository bankAccountRepository)
+        {
+            _bankAccountRepository = bankAccountRepository;
+        }
+
+        public Task<IEnumerable<FileStatusDTO>> ReadTransactionsFromFiles(IFormFileCollection csvFiles, short bankCode)
+        {
+            IBankEstatementReader reader = BankStatementReaderFactory.GetReader(bankCode);
+
+            foreach (var csv in csvFiles)
+            {
+                try
+                {
+                    // TODO: validate file
+
+                    var parsedFile = reader.ProcessBankInformation(csv);
+
+                    PersistBankEstatementInformation(parsedFile);
+                }
+                catch (Exception)
+                {
+                    // TODO: logger
+                }
+            }
+        }
+    }
+}
