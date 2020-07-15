251425c1d8e90f13b02d74575b58c60577585e13
Vitor Filardi <rabelo51@gmail.com>
2020-07-15 20:42:48 -0300

    Add UserConfiguration to DataContext

--- Yaba.Infrastructure.Persistence/Context/DataContext.cs
+++ Yaba.Infrastructure.Persistence/Context/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Yaba.Domain.Models.BankAccounts;
 using Yaba.Domain.Models.Transactions;
+using Yaba.Domain.Models.Users;
 using Yaba.Infrastructure.Persistence.Configurations;
 
 namespace Yaba.Infrastructure.Persistence.Context
@@ -9,6 +10,7 @@ namespace Yaba.Infrastructure.Persistence.Context
     {
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         { }
@@ -18,6 +20,8 @@ namespace Yaba.Infrastructure.Persistence.Context
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
 
             modelBuilder.ApplyConfiguration(new BankAccountConfiguration());
+
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
         }
     }
 }
