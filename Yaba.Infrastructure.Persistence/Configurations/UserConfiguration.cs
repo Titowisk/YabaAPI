61109c7114f7be6189235044757f2c4ab0bd9c83
Vitor Filardi <rabelo51@gmail.com>
2020-07-15 20:02:20 -0300

    Create UserConfiguration

---
+++ Yaba.Infrastructure.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Yaba.Domain.Models.Users;
+
+namespace Yaba.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .ToTable("Users")
+                .HasKey(u => u.Id);
+
+            builder
+                .Property(u => u.Id)
+                .HasColumnName("US_Id")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder
+                .Property(u => u.Name)
+                .HasColumnName("US_Name")
+                .HasMaxLength(256)
+                .IsRequired();
+
+            builder
+                .Property(u => u.Email)
+                .HasColumnName("US_Email")
+                .HasMaxLength(256)
+                .IsRequired();
+
+            builder
+                .Property(u => u.Password)
+                .HasColumnName("US_Password")
+                .HasMaxLength(256)
+                .IsRequired();
+        }
+    }
+}
