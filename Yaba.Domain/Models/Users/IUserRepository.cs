36e59265406f6c4d6d1391b9fef1afb886634db6
Vitor Filardi <rabelo51@gmail.com>
2020-07-17 17:44:45 -0300

    Add UserRepository impl and contract

---
+++ Yaba.Domain/Models/Users/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Yaba.Domain.Models.Users
+{
+    public interface IUserRepository : IAsyncDisposable
+    {
+        Task<User> GetById(int id);
+        Task Create(User entity);
+        Task Update(User entity);
+        Task Delete(int id);
+        Task Delete(User entity);
+    }
+}
---
+++ Yaba.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Yaba.Domain.Models.Users;
+using Yaba.Infrastructure.Persistence.Context;
+
+namespace Yaba.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(User entity)
+        {
+            _context.Users.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _context.Users.FindAsync(id);
+            _context.Users.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(User entity)
+        {
+            _context.Users.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> GetById(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task Update(User entity)
+        {
+            _context.Users.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
