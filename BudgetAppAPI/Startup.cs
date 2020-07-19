49b0267b20662d84337a40adf09302bc8becff01
Vitor Filardi <rabelo51@gmail.com>
2020-07-19 12:35:16 -0300

    Add Jwt configuration to Startup

--- BudgetAppAPI/Startup.cs
+++ BudgetAppAPI/Startup.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using System.Text;
 using Yaba.Application.BankStatementReaders;
 using Yaba.Application.BankStatementReaders.ReaderResolver;
 using Yaba.Application.CsvReaderServices;
@@ -22,12 +25,14 @@ namespace Yaba.WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -48,14 +53,18 @@ namespace Yaba.WebApi
 
             services.AddSingleton<IReaderResolver, ReaderResolver>();
             services.AddTransient<BradescoReader>();
+
+            var secretKey = Configuration.GetSection("JwtConfig:SecretKey").Value;
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+            ConfigureTokenValidation(services, securityKey);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             // $Env:ASPNETCORE_ENVIRONMENT = "Development"
 
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -64,12 +73,34 @@ namespace Yaba.WebApi
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void ConfigureTokenValidation(IServiceCollection services, SecurityKey securityKey)
+        {
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuer = "Yaba API",
+                ValidateAudience = true,
+                ValidAudience = "Yaba API",
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey
+            };
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = _env.IsDevelopment();
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = tokenValidationParameters;
+                });
         }
     }
 }
