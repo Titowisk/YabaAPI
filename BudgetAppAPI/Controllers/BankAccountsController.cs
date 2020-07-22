eab6961bb38c7804dfbcd25aea58b4daad2e576b
Vitor Filardi <rabelo51@gmail.com>
2020-07-22 19:21:45 -0300

    Use bankaccountservice to create bankaccount with logged users

--- BudgetAppAPI/Controllers/BankAccountsController.cs
+++ BudgetAppAPI/Controllers/BankAccountsController.cs
@@ -1,28 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Yaba.Application.BankAccountServices;
 using Yaba.Domain.Models.BankAccounts;
 using Yaba.Domain.Models.BankAccounts.Enumerations;
+using Yaba.Infrastructure.DTO;
 using Yaba.Tools.Validations;
 
 namespace Yaba.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BankAccountsController : ControllerBase
     {
         private readonly ILogger<BankAccountsController> _logger;
+        private readonly IBankAccountService _bankAccountService;
         private readonly IBankAccountRepository _bankAccountRepository;
 
         public BankAccountsController(
             ILogger<BankAccountsController> logger,
-            IBankAccountRepository bankAccountRepository)
+            IBankAccountRepository bankAccountRepository,
+            IBankAccountService bankAccountService)
         {
             _logger = logger;
+            _bankAccountService = bankAccountService;
             _bankAccountRepository = bankAccountRepository;
         }
 
@@ -108,13 +115,13 @@ namespace Yaba.WebApi.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<BankAccount>> PostBankAccount(BankAccount bankAccount)
+        public async Task<ActionResult<BankAccount>> Create(CreateUserBankAccountDTO dto)
         {
             try
             {
-                await _bankAccountRepository.Create(bankAccount);
+                await _bankAccountService.CreateBankAccountForUser(dto);
 
-                return CreatedAtAction("GetBankAccount", new { id = bankAccount.Id }, bankAccount);
+                return Ok("Conta bancária criada com sucesso");
             }
             catch (ArgumentException aex)
             {
