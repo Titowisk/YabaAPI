3faff530cd1e08623ee27669c1ab84cab72654b3
Vitor Filardi <rabelo51@gmail.com>
2020-07-22 19:20:15 -0300

    Create BankAccountService

---
+++ Yaba.Application/BankAccountServices/IBankAccountService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Yaba.Infrastructure.DTO;
+
+namespace Yaba.Application.BankAccountServices
+{
+    public interface IBankAccountService
+    {
+        Task CreateBankAccountForUser(CreateUserBankAccountDTO dto);
+    }
+}
---
+++ Yaba.Application/BankAccountServices/Impl/BankAccountService.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Yaba.Domain.Models.BankAccounts;
+using Yaba.Domain.Models.BankAccounts.Enumerations;
+using Yaba.Domain.Models.Users;
+using Yaba.Infrastructure.DTO;
+using Yaba.Tools.Validations;
+
+namespace Yaba.Application.BankAccountServices.Impl
+{
+    public class BankAccountService : IBankAccountService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IBankAccountRepository _bankAccountRepository;
+
+        public BankAccountService(
+            IUserRepository userRepository,
+            IBankAccountRepository bankAccountRepository)
+        {
+            _userRepository = userRepository;
+            _bankAccountRepository = bankAccountRepository;
+        }
+        public async Task CreateBankAccountForUser(CreateUserBankAccountDTO dto)
+        {
+            var user = await _userRepository.GetById(dto.UserId);
+            Validate.NotNull(user, "É necessário um usuário para criar uma conta de banco.");
+
+            Validate.NotNullOrEmpty(dto.Agency, "É necessário fornecer uma agência.");
+
+            Validate.NotNullOrEmpty(dto.Number, "É necessário fornecer o número da conta bancária.");
+
+            BankCode.ValidateCode(dto.Code);
+
+            var code = BankCode.FromValue<BankCode>(dto.Code);
+
+            var bankAccount = new BankAccount(dto.Number, dto.Agency, code, dto.UserId);
+
+            await _bankAccountRepository.Create(bankAccount);
+        }
+    }
+}
