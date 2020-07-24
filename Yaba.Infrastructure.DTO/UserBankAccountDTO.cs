fe3804ce1f452516ef72f5841eabc521493ddaf5
Vitor Filardi <rabelo51@gmail.com>
2020-07-24 20:38:32 -0300

    Create UserBankAccountDTO

--- Yaba.Infrastructure.DTO/UserBankAccountDTO.cs
+++ Yaba.Infrastructure.DTO/UserBankAccountDTO.cs
@@ -7,4 +7,12 @@
         public string Agency { get; set; }
         public short Code { get; set; }
     }
+
+    public class UserBankAccountDTO 
+    {
+        public int UserId { get; set; }
+        public string Number { get; set; }
+        public string Agency { get; set; }
+        public short Code { get; set; }
+    }
 }
