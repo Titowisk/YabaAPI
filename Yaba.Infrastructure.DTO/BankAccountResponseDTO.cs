01a5516dd39e4393b468bd8c2921dea3a1360f53
Vitor Filardi <rabelo51@gmail.com>
2020-07-25 15:11:52 -0300

    Create BankAccountResponseDTO

---
+++ Yaba.Infrastructure.DTO/BankAccountResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Yaba.Infrastructure.DTO
+{
+    public class BankAccountResponseDTO
+    {
+        public int Id { get; set; }
+        public short BankCode { get; set; }
+        public string BankName { get; set; }
+        public string Agency { get; set; }
+        public string AccountNumber { get; set; }
+    }
+}
