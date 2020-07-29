baf19c1c0ec27a8eca20853b107cac5ea7d491f7
Vitor Filardi <rabelo51@gmail.com>
2020-07-29 18:34:52 -0300

    Create TransactionsDateFilterResponseDTO

---
+++ Yaba.Infrastructure.DTO/TransactionsResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Yaba.Infrastructure.DTO
+{
+    public class TransactionsDateFilterResponseDTO
+    {
+        public string Origin { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
