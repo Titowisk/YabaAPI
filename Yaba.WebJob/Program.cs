531bfa0e8193e9a0756bd45be97a25bb5430ba5a
Vitor Filardi <rabelo51@gmail.com>
2020-08-22 15:50:19 -0300

    Create project Yaba.WebJob

---
+++ Yaba.WebJob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yaba.WebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
