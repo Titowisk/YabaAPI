4e322b5de8d02ff4a265c91d61a7916e5742ed48
Vitor Filardi <rabelo51@gmail.com>
2020-08-22 16:22:16 -0300

    Add builder and host

--- Yaba.WebJob/Program.cs
+++ Yaba.WebJob/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Yaba.WebJob
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
         }
     }
 }
