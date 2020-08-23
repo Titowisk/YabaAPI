50c9c552df3a5df02539adb4588a9ebe0b7bb508
Vitor Filardi <rabelo51@gmail.com>
2020-08-23 15:53:30 -0300

    Add transaction service injection

--- Yaba.WebJob/Functions.cs
+++ Yaba.WebJob/Functions.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Yaba.Application.TransactionServices;
 
 namespace Yaba.WebJob
 {
     public class Functions
     {
-        public static void ProcessQueueMessage([QueueTrigger("yabadev")] string message, ILogger logger)
+        private readonly ITransactionService _transactionService;
+
+        public Functions(ITransactionService transactionService)
         {
+            _transactionService = transactionService;
+        }
+
+        public void ProcessQueueMessage([QueueTrigger("yabadev")] string message, ILogger logger)
+        {
+            _transactionService.GetType();
             logger.LogInformation(message);
         }
+
+        //public static void ProcessQueueMessage([QueueTrigger("yabadev")] string message, ILogger logger)
+        //{
+        //    logger.LogInformation(message);
+        //}
     }
 }
