e84b9e3d48dbaf41e538e8f01595e775d059a47e
Vitor Filardi <rabelo51@gmail.com>
2020-09-03 20:49:37 -0300

    Add column TR_Metadata + migration

---
+++ Yaba.Infrastructure.Persistence/Migrations/20200903234609_Add_Transaction_Metadata.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Yaba.Infrastructure.Persistence.Migrations
+{
+    public partial class Add_Transaction_Metadata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TR_Metadata",
+                table: "Transactions",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TR_Metadata",
+                table: "Transactions");
+        }
+    }
+}
