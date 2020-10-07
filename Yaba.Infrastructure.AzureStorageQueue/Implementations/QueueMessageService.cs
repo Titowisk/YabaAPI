e4b378acd839b47785774a951eeddad93bc3f28b
Vitor Filardi <rabelo51@gmail.com>
2020-10-07 19:31:54 -0300

    Add user secrets to WebAPI

--- Yaba.Infrastructure.AzureStorageQueue/Implementations/QueueMessageService.cs
+++ Yaba.Infrastructure.AzureStorageQueue/Implementations/QueueMessageService.cs
@@ -8,16 +8,18 @@ namespace Yaba.Infrastructure.AzureStorageQueue.Implementations
     public class QueueMessageService : IQueueMessageService
     {
         private readonly AzureConfig _option;
+        private readonly ConnectionStrings _connectionStrings;
 
-        public QueueMessageService(IOptions<AzureConfig> option)
+        public QueueMessageService(IOptions<AzureConfig> option, IOptions<ConnectionStrings> connectionStrings)
         {
             _option = option.Value;
+            _connectionStrings = connectionStrings.Value;
         }
 
         public void SendMessage(string message)
         {
             // Instantiate a QueueClient which will be used to create and manipulate the queue
-            QueueClient queueClient = new QueueClient(_option.AzureWebJobsStorage, _option.QueueName);
+            QueueClient queueClient = new QueueClient(_connectionStrings.AzureWebJobsStorage, _option.QueueName);
 
             if (queueClient.Exists())
             {
