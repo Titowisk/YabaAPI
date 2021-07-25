25501b6bef385082afa1c3f4641b359314e81004
Vitor Filardi <rabelo51@gmail.com>
2021-07-25 16:23:18 -0300

    Create DependencyContainer

---
+++ test/Yaba.Tests/DependencyInversion/DependencyContainer.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yaba.Application.BankAccountServices;
+using Yaba.Application.BankAccountServices.Impl;
+using Yaba.Application.BankStatementReaders;
+using Yaba.Application.BankStatementReaders.ReaderResolver;
+using Yaba.Application.CsvReaderServices;
+using Yaba.Application.CsvReaderServices.Impl;
+using Yaba.Application.TransactionServices;
+using Yaba.Application.TransactionServices.Impl;
+using Yaba.Application.UserServices;
+using Yaba.Application.UserServices.Impl;
+using Yaba.Domain.Models.BankAccounts;
+using Yaba.Domain.Models.Transactions;
+using Yaba.Domain.Models.Users;
+using Yaba.Infrastructure.AzureStorageQueue.Contracts;
+using Yaba.Infrastructure.AzureStorageQueue.Implementations;
+using Yaba.Infrastructure.Persistence.Context;
+using Yaba.Infrastructure.Persistence.Repositories;
+using Yaba.Infrastructure.Persistence.UnitOfWork;
+
+namespace Yaba.Tests.DependencyInversion
+{
+    public static class DependencyContainer
+    {
+        public static IServiceProvider GetServicesUsingSQLite()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var sqliteOptions = new DbContextOptionsBuilder<DataContext>().UseSqlite("Filename=Test.db").Options;
+            //services.AddDbContext<DataContext>(s => new DataContext(sqliteOptions));
+            //var sqliteContext = new DataContext(sqliteOptions);
+            //sqliteContext.Database.EnsureDeleted();
+            //sqliteContext.Database.EnsureCreated();
+            services.AddScoped(_ => {
+                var context = new DataContext(sqliteOptions);
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                return context;
+            });
+
+            GetDomainCollection(services);
+            GetApplicationCollection(services);
+            GetInfrastructureCollection(services);
+
+            return services.BuildServiceProvider();
+        }
+
+        private static void GetInfrastructureCollection(IServiceCollection services)
+        {
+            services.AddScoped<IQueueMessageService, QueueMessageService>(); //TODO Mock
+        }
+
+        private static void GetApplicationCollection(IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IBankAccountService, BankAccountService>();
+            services.AddScoped<ITransactionService, TransactionService>();
+            services.AddScoped<ICsvReaderService, CsvReaderService>();
+
+            services.AddSingleton<IReaderResolver, ReaderResolver>();
+            services.AddTransient<BradescoReader>();
+        }
+
+        private static void GetDomainCollection(IServiceCollection services)
+        {
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<IBankAccountRepository, BankAccountRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<UnitOfWork>();
+        }
+    }
+}
