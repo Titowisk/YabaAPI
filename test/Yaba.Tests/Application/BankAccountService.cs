82aa08685ff21ecabfacef89bdda4cde0c3c82e1
Vitor Filardi <rabelo51@gmail.com>
2021-08-01 10:38:16 -0300

    Create test DeleteBankAccount_DeleteUsersBankAccount

--- test/Yaba.Tests/Application/BankAccountService.cs
+++ test/Yaba.Tests/Application/BankAccountService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 using Yaba.Application.BankAccountServices;
 using Yaba.Domain.Models.BankAccounts;
 using Yaba.Domain.Models.BankAccounts.Enumerations;
@@ -15,8 +16,18 @@ using Yaba.Tests.DependencyInversion;
 
 namespace Yaba.Tests.Application
 {
+    // TODO: Create another FACT and check the behave of database context: is it really totally separeted?
+    // Or are they sharing the same context? (see AddScoped DataContext)
+    // Check how Xunit make parallel tests
     public class BankAccountService
     {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public BankAccountService(ITestOutputHelper outputHelper)
+        {
+            this._outputHelper = outputHelper;
+        }
+
         [Fact]
         public async void CreateBankAccountForUser_CreatesOneBankAccountToExistingUser()
         {
@@ -40,9 +51,43 @@ namespace Yaba.Tests.Application
 
             // then the existing user should have a new BankAccount
             var bankAccountRepository = (IBankAccountRepository)serviceProvider.GetService(typeof(IBankAccountRepository));
-            var bankAccount = await bankAccountRepository.GetBy("123456", "12345678", BankCode.GENERICBANK.Value);
+            //var bankAccount = await bankAccountRepository.GetBy("123456", "12345678", BankCode.GENERICBANK.Value);
+            var bankAccount = await bankAccountRepository.GetById(1); // this should work since each Fact has an isolated sqlite context
 
             Assert.Equal(user.Id, bankAccount.UserId);
+        }
+
+        [Fact]
+        public async void DeleteBankAccount_DeleteUsersBankAccount()
+        {
+            var serviceProvider = DependencyInversion.DependencyContainer.GetServicesUsingSQLite();
+
+            // given an existing user with one bank account
+            var userRepository = (IUserRepository)serviceProvider.GetService(typeof(IUserRepository));
+            var user = new User("test", "test@email.com", "123123");
+            await userRepository.Create(user);
+
+            var bankAccountService = (IBankAccountService)serviceProvider.GetService(typeof(IBankAccountService));
+            var dto = new CreateUserBankAccountDTO
+            {
+                UserId = user.Id,
+                Agency = "123456",
+                Code = BankCode.GENERICBANK.Value,
+                Number = "12345678"
+            };
+            await bankAccountService.CreateBankAccountForUser(dto);
+
+            // when service DeleteBankAccount is called
+            var bankAccountRepository = (IBankAccountRepository)serviceProvider.GetService(typeof(IBankAccountRepository));
+            var bankAccount = await bankAccountRepository.GetById(1); // this should work since each Fact has an isolated sqlite context
+
+            var deleteDTO = new DeleteUserBankAccountDTO { BankAccountId = bankAccount.Id, UserId = user.Id };
+            await bankAccountService.DeleteBankAccount(deleteDTO);
+
+            // then the user's bankaccount will be deleted
+            var result = await bankAccountRepository.GetAllByUser(user.Id);
+
+            Assert.True(!result.Any());
         }
     }
 }
