ba95d4be1c5a85c703e16b4e7e473fe3d86ae266
Vitor Filardi <rabelo51@gmail.com>
2021-08-07 15:56:41 -0300

    Creates BankAccountBuilder

--- test/Yaba.Tests/Application/BankAccountService.cs
+++ test/Yaba.Tests/Application/BankAccountService.cs
@@ -8,6 +8,7 @@ using Yaba.Domain.Models.BankAccounts.Enumerations;
 using Yaba.Domain.Models.Users;
 using Yaba.Infrastructure.DTO;
 using Yaba.Infrastructure.Persistence.UnitOfWork;
+using Yaba.Tests.EntitiesCreator.BankAccountEntity;
 
 namespace Yaba.Tests.Application
 {
@@ -50,7 +51,6 @@ namespace Yaba.Tests.Application
 
             // then the existing user should have a new BankAccount
             var bankAccountRepository = (IBankAccountRepository)serviceProvider.GetService(typeof(IBankAccountRepository));
-            //var bankAccount = await bankAccountRepository.GetBy("123456", "12345678", BankCode.GENERICBANK.Value);
             var bankAccount = await bankAccountRepository.GetById(1); // this should work since each Fact has an isolated sqlite context
 
             _outputHelper.WriteLine("======CreateBankAccountForUser_CreatesOneBankAccountToExistingUser");
@@ -65,29 +65,19 @@ namespace Yaba.Tests.Application
             var serviceProvider = DependencyInversion.DependencyContainer.GetServicesUsingSQLite(_sqlite_db_filename);
 
             // given an existing user with one bank account
-            var userRepository = (IUserRepository)serviceProvider.GetService(typeof(IUserRepository));
-            var user = new User("test", "test@email.com", "123123");
-            await userRepository.Create(user);
+            var entities = await BankAccountBuilder
+                                    .Create(serviceProvider)
+                                    .Build();
 
             var bankAccountService = (IBankAccountService)serviceProvider.GetService(typeof(IBankAccountService));
-            var dto = new CreateUserBankAccountDTO
-            {
-                UserId = user.Id,
-                Agency = "123456",
-                Code = BankCode.GENERICBANK.Value,
-                Number = "12345678"
-            };
-            await bankAccountService.CreateBankAccountForUser(dto);
 
             // when service DeleteBankAccount is called
-            var bankAccountRepository = (IBankAccountRepository)serviceProvider.GetService(typeof(IBankAccountRepository));
-            var bankAccount = await bankAccountRepository.GetById(1); // this should work since each Fact has an isolated sqlite context
-
-            var deleteDTO = new DeleteUserBankAccountDTO { BankAccountId = bankAccount.Id, UserId = user.Id };
+            var deleteDTO = new DeleteUserBankAccountDTO { BankAccountId = entities.BankAccount.Id, UserId = entities.User.Id };
             await bankAccountService.DeleteBankAccount(deleteDTO);
 
             // then the user's bankaccount will be deleted
-            var result = await bankAccountRepository.GetAllByUser(user.Id);
+            var bankAccountRepository = (IBankAccountRepository)serviceProvider.GetService(typeof(IBankAccountRepository));
+            var result = await bankAccountRepository.GetAllByUser(entities.User.Id);
 
             _outputHelper.WriteLine("======DeleteBankAccount_DeleteUsersBankAccount");
             _outputHelper.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
---
+++ test/Yaba.Tests/EntitiesCreator/EntitiesCreatorResponse.cs
@@ -0,0 +1,12 @@
+﻿using Yaba.Domain.Models.BankAccounts;
+using Yaba.Domain.Models.Users;
+
+namespace Yaba.Tests.EntitiesCreator
+{
+    public class EntitiesCreatorResponse
+    {
+        public BankAccount BankAccount { get; set; }
+
+        public User User { get; set; }
+    }
+}
