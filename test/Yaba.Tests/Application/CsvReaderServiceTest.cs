6b426839d84166120f7571f2164fd23ce6477719
Vitor Filardi <rabelo51@gmail.com>
2021-08-29 13:34:32 -0300

    test: create ReadTransactionsFromFiles_ReadTheSameFileTwice test

--- test/Yaba.Tests/Application/CsvReaderServiceTest.cs
+++ test/Yaba.Tests/Application/CsvReaderServiceTest.cs
@@ -1,15 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Yaba.Application.CsvReaderServices;
-using Yaba.Domain.Models.BankAccounts;
 using Yaba.Domain.Models.BankAccounts.Enumerations;
 using Yaba.Infrastructure.DTO;
 using Yaba.Infrastructure.Persistence.Context;
@@ -19,17 +16,18 @@ namespace Yaba.Tests.Application
 {
     public class CsvReaderServiceTest
     {
-        [Fact]
+        [Fact(DisplayName = "Service ReadTransactionsFromFiles should read a csv file from a nubank's card statement")]
         public async Task ReadTransactionsFromFiles_NubankCsvFile()
         {
             var serviceProvider = DependencyInversion.DependencyContainer.GetServicesUsingSQLite("CsvReaderServiceTest");
+
             // given a bankAccount
-            //  and a card statement from nubank with 10 expenses
             var response = BankAccountBuilder
                 .CreateABankAccount(serviceProvider)
                 .WithBankCode(BankCode.NUBANK)
                 .Build();
 
+            //  and a card statement from nubank with 10 expenses
             var csvFile = GetCsvFile("ProcessBankInformation_1"); // TODO: Duplicated Code -> refactor
             Mock<IFormFile> fileMock = MockFile(csvFile);
 
@@ -59,6 +57,57 @@ namespace Yaba.Tests.Application
             var context = (DataContext)serviceProvider.GetService(typeof(DataContext));
             var bankAccount = context.BankAccounts.Include(bk => bk.Transactions).First(bk => bk.Id == response.BankAccount.Id);
             Assert.Equal(10, bankAccount.Transactions.Count);
+        }
+
+        [Fact(DisplayName = "ReadTransactionsFromFiles should not duplicate transactions if the same transactions are read again")]
+        public async Task ReadTransactionsFromFiles_ReadTheSameFileTwice()
+        {
+            var serviceProvider = DependencyInversion.DependencyContainer.GetServicesUsingSQLite("CsvReaderServiceTest");
+
+            // given a bankAccount
+            var response = BankAccountBuilder
+                .CreateABankAccount(serviceProvider)
+                .WithBankCode(BankCode.NUBANK)
+                .Build();
+
+            //  and a card statement from nubank with 10 expenses
+            var csvFile = GetCsvFile("ProcessBankInformation_1"); // TODO: Duplicated Code -> refactor
+            Mock<IFormFile> fileMock = MockFile(csvFile);
+
+            var fileCollectionMock = new Mock<IFormFileCollection>();
+            var fileList = new List<IFormFile>() { fileMock.Object };
+            fileCollectionMock.Setup(_ => _.GetEnumerator()).Returns(fileList.GetEnumerator());
+
+            // when I use service ReadTransactionsFromFiles for the second time with the same file
+            var csvReaderService = (ICsvReaderService)serviceProvider.GetService(typeof(ICsvReaderService));
+            var dto = new CsvFileReaderDTO
+            {
+                BankAccountId = response.BankAccount.Id,
+                FilesOwnerId = response.User.Id,
+                CsvFiles = fileCollectionMock.Object
+            };
+            await csvReaderService.ReadTransactionsFromFiles(dto);
+
+            var secondFileMock = MockFile(csvFile);
+
+            var secondFileCollectionMock = new Mock<IFormFileCollection>();
+            var secondFileList = new List<IFormFile>() { secondFileMock.Object };
+            secondFileCollectionMock.Setup(_ => _.GetEnumerator()).Returns(secondFileList.GetEnumerator());
+            var secondDto = new CsvFileReaderDTO
+            {
+                BankAccountId = response.BankAccount.Id,
+                FilesOwnerId = response.User.Id,
+                CsvFiles = secondFileCollectionMock.Object
+            };
+            var secondResult = await csvReaderService.ReadTransactionsFromFiles(secondDto);
+
+            // then the result of reading should be successul but fileResult should show 10 skiped transactions
+            Assert.Single(secondResult);
+            var fileStatus = secondResult.First();
+            Assert.True(fileStatus.IsSuccessfullRead);
+            Assert.Equal(10, fileStatus.TransactionsRead);
+            Assert.Equal(0, fileStatus.TransactionsSaved);
+            Assert.Equal(10, fileStatus.ExistentTransactionsSkiped);
         }
 
         #region Priv Methods
