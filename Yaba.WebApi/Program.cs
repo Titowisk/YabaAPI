cbd8d6ef6dce5b99c9fada0102dc0ab2767506af
Titowisk <rabelo51@gmail.com>
2024-03-30 11:15:05 -0300

    refactor: create methods for organization

--- Yaba.WebApi/Program.cs
+++ Yaba.WebApi/Program.cs
@@ -21,55 +21,11 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
 builder.Logging.SetMinimumLevel(LogLevel.Trace);
 
-builder.Services
-    .Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
-builder.Services
-    .Configure<AzureConfig>(builder.Configuration.GetSection("AzureConfig"));
-builder.Services
-    .Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
+ConfigServiceCollection(builder);
 
-builder.Services.AddControllers()
-    .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(swaggerOptions =>
-{
-    swaggerOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "YabaAPI", Version = "v1" });
-});
-
-DependencyResolver.RegisterServices(builder.Services, builder.Configuration);
-
-var secretKey = builder.Configuration.GetSection("JwtConfig:SecretKey").Value;
-var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
-ConfigureTokenValidation(builder.Services, securityKey);
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policyBuilder =>
-    {
-        policyBuilder.AllowAnyHeader();
-        policyBuilder.AllowAnyMethod();
-        policyBuilder.AllowAnyMethod();
-    });
-});
-
-WebApplication app = builder.Build();
 // <
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseHttpsRedirection();
-app.UseSwagger();
-app.UseSwaggerUI(swaggerOptions => swaggerOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "YabaAPI V1"));
-
-app.UseRouting();
-
-app.UseCors();
-
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+WebApplication app = builder.Build();
+ConfigWebApplication(app);
 
 void ConfigureTokenValidation(IServiceCollection services, SecurityKey securityKey)
 {
@@ -90,4 +46,58 @@ void ConfigureTokenValidation(IServiceCollection services, SecurityKey securityK
             options.SaveToken = true;
             options.TokenValidationParameters = tokenValidationParameters;
         });
+}
+
+static void ConfigWebApplication(WebApplication app)
+{
+    app.UseMiddleware<ExceptionMiddleware>();
+    app.UseHttpsRedirection();
+    app.UseSwagger();
+    app.UseSwaggerUI(swaggerOptions => swaggerOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "YabaAPI V1"));
+
+    app.UseRouting();
+
+    app.UseCors();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
+
+void ConfigServiceCollection(WebApplicationBuilder builder)
+{
+    builder.Services
+        .Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+    builder.Services
+        .Configure<AzureConfig>(builder.Configuration.GetSection("AzureConfig"));
+    builder.Services
+        .Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
+
+    builder.Services.AddControllers()
+        .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(swaggerOptions =>
+    {
+        swaggerOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "YabaAPI", Version = "v1" });
+    });
+
+    DependencyResolver.RegisterServices(builder.Services, builder.Configuration);
+
+    var secretKey = builder.Configuration.GetSection("JwtConfig:SecretKey").Value;
+    var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+    ConfigureTokenValidation(builder.Services, securityKey);
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(policyBuilder =>
+        {
+            policyBuilder.AllowAnyHeader();
+            policyBuilder.AllowAnyMethod();
+            policyBuilder.AllowAnyMethod();
+        });
+    });
 }
