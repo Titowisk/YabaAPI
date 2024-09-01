bc0f30e557ddb05eb2d53b3c2ad5cbd9a88872ef
Titowisk <rabelo51@gmail.com>
2024-09-01 13:23:17 -0300

    feat: create TransactionCategoryWorker

---
+++ Yaba.TransactionCategoryWorker/Program.cs
@@ -0,0 +1,8 @@
+using Yaba.TransactionCategoryWorker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+builder.Services.AddHostedService<Worker2>();
+
+var host = builder.Build();
+host.Run();
