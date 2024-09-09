58743e842ab344e3b43f35b640fa255b1506e3ac
Titowisk <rabelo51@gmail.com>
2024-09-08 21:31:53 -0300

    fix: user generation from seeder + change the quantity of transactions generated

--- Yaba.Infrastructure.Persistence/Context/Seeder.cs
+++ Yaba.Infrastructure.Persistence/Context/Seeder.cs
@@ -7,6 +7,7 @@ using Yaba.Domain.Models.BankAccounts.Enumerations;
 using Yaba.Domain.Models.Transactions;
 using Yaba.Domain.Models.Transactions.Enumerations;
 using Yaba.Domain.Models.Users;
+using Yaba.Infrastructure.Security;
 
 namespace Yaba.Infrastructure.Persistence.Context
 {
@@ -18,7 +19,8 @@ namespace Yaba.Infrastructure.Persistence.Context
             if (doesUserExist)
                 return;
 
-            User testUser = new("test-user", "test-seed@gmail.com", "123Correct_");
+            
+            User testUser = new("test-user", "test-seed@gmail.com", SecurityManager.GeneratePbkdf2Hash("123Correct_"));
             dataContext.Users.Add(testUser);
             dataContext.SaveChanges();
 
@@ -28,12 +30,12 @@ namespace Yaba.Infrastructure.Persistence.Context
 
             List<Transaction> transactions = new Faker<Transaction>()
                 .RuleFor(t => t.Origin, f => f.Company.CompanyName())
-                .RuleFor(t => t.Date, (f, t) => f.Date.Past(2, new DateTime(2024, 3, 15)))
+                .RuleFor(t => t.Date, (f, t) => f.Date.Past(4, new DateTime(2024, 3, 15)))
                 .RuleFor(t => t.Amount, (f, t) => f.Finance.Amount(3.00M, 467.88M))
                 .RuleFor(t => t.BankAccountId, account.Id)
                 .RuleFor(t => t.Category, f => f.Random.Enum<Category>())
                 .RuleFor(t => t.Metadata, f => $"GenericBank_{Guid.NewGuid()}")
-                .Generate(100)
+                .Generate(1000)
                 ;
             dataContext.Transactions.AddRange(transactions);
 
